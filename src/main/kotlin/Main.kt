f8baf1c709b6dacb5a38653d0b7d5c34fa329a57
Benjimg2783 <bmartinez@iesrafaelalberti.es>
2021-12-15 20:31:50 +0100

    terminado

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,12 +8,18 @@ interface ArmasFuego {
     }
 }
 
-class Casa(val direccion: String, override var municion: Int, override val municionARestar: Int) : ArmasFuego
+class Casa(private val direccion: String, override var municion: Int,override val municionARestar: Int, ) : ArmasFuego
 class Coche(private val matricula: String, override var municion: Int, override val municionARestar: Int) : ArmasFuego
+class Bocadillo(private val sabor:String,override var municion: Int,override val municionARestar: Int):ArmasFuego
 
 fun main() {
     val casa = Casa("Avd.Segunda Aguanda 10", 30, 5)
     val coche = Coche("A123", 20, 3)
-    println(coche.disparar())
-    println(casa.disparar())
+    val bocadillo=Bocadillo("chorizo",10,2)
+    val lista = listOf(casa,coche,bocadillo)
+    val disparos = mutableMapOf<String, ArmasFuego>()
+    for (i in 0 until 6) {
+        disparos["$i"] = lista.random()
+    }
+    disparos.mapValues { it.value.disparar(); println(it.value) }
 }
